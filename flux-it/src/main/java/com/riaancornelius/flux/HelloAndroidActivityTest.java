597de84c60eebf7957b348af402ab51b801f5492
Riaan Cornelius <riaan@rdcreative.co>
2013-11-17 22:01:15 +0200

    Flux android app - Initial commit of project with testing library set up

---
+++ flux-it/src/main/java/com/riaancornelius/flux/HelloAndroidActivityTest.java
@@ -0,0 +1,16 @@
+package com.riaancornelius.flux;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+public class HelloAndroidActivityTest extends ActivityInstrumentationTestCase2<HelloAndroidActivity> {
+
+    public HelloAndroidActivityTest() {
+        super(HelloAndroidActivity.class); 
+    }
+
+    public void testActivity() {
+        HelloAndroidActivity activity = getActivity();
+        assertNotNull(activity);
+    }
+}
+
